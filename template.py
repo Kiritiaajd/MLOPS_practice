d37dcaa784e2379b14932bec7078284fbf0d6fe9
Kiriti Aajad <kiriti.aajad@gmail.com>
2024-10-27 10:55:06 +0530

    template

---
+++ template.py
@@ -0,0 +1,28 @@
+import os
+from pathlib import Path
+
+list_of_files = [
+    ".github\workflows",
+    "src\__init__.py",
+    "src\components\data_ingestion.py",
+    "src\components\data_tranformation.py",
+    "src\components\model_traines.py",
+    "src\components/model_evaluation.py",
+    "src\pipeline\__init__.py",
+    "src\pipeline\training.py",
+    "src\pipeline\prediction_pipeline.py",
+    "src\utils\utils.py",
+    "tests\unit\__init__.py" ,
+    "tests\integration\__init.py",
+    "init_setup.sh",
+    "requirments.txt",
+    "requirmnets_dev.txt",
+    "setup.py",
+    "setup.cfg",
+    "py_project.toml"
+    "tox.ini",
+    "experiment/experiments.ipynb" 
+    
+    
+    
+]
